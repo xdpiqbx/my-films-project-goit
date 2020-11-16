d5c54d9beab188be7351dd6eeb20a7d2c86bdd30
xdpiqbx <xdpiqbx@gmail.com>
2020-11-16 21:37:19 +0200

    added api key and first fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './styles.css';
+import fetchFilmsPromise from './javascript/fetchFilms'
+import './styles.scss';
+
+const refs = {
+    mainWrapper: document.querySelector('.js-films-wrapper-list')
+}
+
+fetchFilmsPromise.fetchFilms()
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data.results.map(d => d.title || d.name))
+        // console.log(data.results.map(d => d.backdrop_path))
+        // console.log(data.results.map(d => d.poster_path))
+        // console.log(data.results.map(d => d))
+
+        data.results.map(d => {
+            d.poster_path
+            const html = `
+                <li class="js-films-wrapper-list__item">
+                    <img width="200px" src="https://image.tmdb.org/t/p/w500/${d.poster_path}"/>
+                    <p>${d.title || d.name}</p>
+                </li>                
+            `
+            refs.mainWrapper.insertAdjacentHTML("beforeend", html)
+        })
+
+    })
